4cc0a5503ed8c98b25a33c10a92fcab23b7e7475
Ankit Garg <ankit.garg1398@gmail.com>
2024-12-21 17:12:10 +0530

    Add project files.

---
+++ TweetApp.Api/MessegeSender/KafkaSender.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace TweetApp.MessegeSender
+{
+    public class KafkaSender : IKafkaSender
+    {
+        async public void Publish(string message)
+        {
+            var conStr = "Endpoint=sb://tweetservicebus.servicebus.windows.net/;SharedAccessKeyName=TweetAppPolicy;SharedAccessKey=nkWpZwH1E4Cv9F3hnrHQdzDYWYvl/GN/RFozH5CODbA=;EntityPath=tweetapptopic";
+            var client = new ServiceBusClient(conStr);
+            var sender = client.CreateSender("tweetapptopic");
+            var Message = new ServiceBusMessage(message);
+            await sender.SendMessageAsync(Message);
+
+        }
+    }
+}
